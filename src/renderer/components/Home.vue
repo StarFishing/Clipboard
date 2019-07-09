0d0e229e7933ef22ff083a78753602fe9ff281af
startfishing <2923627211@qq.com>
2019-07-10 00:31:18 +0800

    修改配色

---
+++ src/renderer/components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="homepage">
+    <Sidebar></Sidebar>
+  </div>
+</template>
+<script>
+import Sidebar from '@/components/Sidebar/index'
+export default {
+  data () {
+    return {}
+  },
+  components: {
+    Sidebar
+  }
+}
+</script>
+
+<style>
+.homepage {
+  background-color: antiquewhite;
+  position: absolute;
+  top: 50px;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: linear-gradient(180deg, #7a5e87, #393b50);
+}
+</style>
