52078ef825b637397b7741e7105582a291519e84
starfishing <2923627211@qq.com>
2019-07-10 17:35:51 +0800

    配置子路由位置

--- src/renderer/components/Home.vue
+++ src/renderer/components/Home.vue
@@ -1,16 +1,26 @@
 <template>
   <div class="homepage">
     <Sidebar></Sidebar>
+    <transition name="fade">
+      <router-view :key="key" />
+    </transition>
   </div>
 </template>
 <script>
 import Sidebar from '@/components/Sidebar/index'
+import Clipboard from '@/components/Clipboard/index'
 export default {
   data () {
     return {}
   },
+  computed: {
+    key () {
+      return this.$route.path
+    }
+  },
   components: {
-    Sidebar
+    Sidebar,
+    Clipboard
   }
 }
 </script>
@@ -23,6 +33,28 @@ export default {
   bottom: 0;
   left: 0;
   right: 0;
+  overflow: hidden;
   background: linear-gradient(180deg, #7a5e87, #393b50);
+}
+.fade-enter-to,
+.fade-leave {
+  opacity: 1;
+  transform: translateY(0);
+}
+
+.fade-enter {
+  transform: translateY(100%);
+  opacity: 0;
+}
+.fade-leave-to {
+  transform: translateY(-100%);
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: all 0.3s ease;
+}
+.fade-leave-active {
+  transition: all 0.1s ease;
 }
 </style>
