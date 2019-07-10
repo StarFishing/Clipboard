5670c30ece46f1b25904a2ddd0e09c177ec940c1
startfishing <2923627211@qq.com>
2019-07-10 23:11:57 +0800

    还原托盘设置

--- src/main/tray.js
+++ src/main/tray.js
@@ -1,4 +1,16 @@
-module.exports = function () {
+// module.exports = (function () {
+//   const path = require('path')
+//   const { BrowserWindow, Tray } = require('electron')
+//   // BrowserWindow
+//   // 创建系统托盘
+//   let tray = new Tray(path.resolve(__static, 'favicon-a.png'))
+//   var win = BrowserWindow.getAllWindows()
+//   tray.setToolTip('star pages')
+//   tray.on('click', () => {
+//     win[0].show()
+//   })
+// })
+;(function () {
   const path = require('path')
   const { BrowserWindow, Tray } = require('electron')
   // BrowserWindow
@@ -9,4 +21,4 @@ module.exports = function () {
   tray.on('click', () => {
     win[0].show()
   })
-}
+})()
