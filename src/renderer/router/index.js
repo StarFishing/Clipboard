0e8da8c1089de213441f2c3a261ab45b87903bbd
starfishing <2923627211@qq.com>
2019-07-10 17:33:46 +0800

    为视图配置路由

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -1,14 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+// import Clipboard from '@/components/Clipboard/index'
 Vue.use(Router)
-
+const Clipboard = () => import('@/components/Clipboard/index')
+const Home = () => import('@/components/Home')
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'index',
-      component: require('@/components/Home').default
+      name: 'Home',
+      component: Home,
+      children: [
+        {
+          path: '/Clipboard',
+          name: 'Clipboard',
+          component: Clipboard
+        },
+        {
+          path: '/TaskList',
+          name: 'TaskList',
+          component: () => import('@/components/TaskList/index')
+        }
+      ]
     },
     {
       path: '*',
