e2afa46632282f724a6e546a1c91d4dc86831064
starfishing <2923627211@qq.com>
2019-07-11 17:54:46 +0800

    clip

---
+++ src/main/Files/ClipFile.js
@@ -0,0 +1,11 @@
+// const fs = require('fs')
+const routes = require('../.././utils/creatpath')
+routes.checkDirExist('./Clipdata/data')
+// fs.mkdir('D:Clipdata', { recursive: true }, err => {
+//   if (err) throw err
+//   fs.writeFile('D:Clipdatastar.txt', 'hello', 'utf8', err => {
+//     if (err) throw err
+//     console.log('文件已被保存')
+//   })
+//   console.log('path success')
+// })
