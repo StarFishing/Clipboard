a4a731036f8217704019867f140b5d30c130269b
startfishing <2923627211@qq.com>
2019-07-12 00:08:16 +0800

    添加任务列表原型

--- src/renderer/components/TaskList/index.vue
+++ src/renderer/components/TaskList/index.vue
@@ -1,139 +1,259 @@
 <template>
-  <div>
-    <section class="todoapp">
-      <!-- header -->
-      <header class="header">
-        <input class="new-todo"
-               autocomplete="off"
-               placeholder="Todo List"
-               @keyup.enter="addTodo">
-      </header>
-      <!-- main section -->
-      <section v-show="todos.length"
-               class="main">
-        <input id="toggle-all"
-               :checked="allChecked"
-               class="toggle-all"
-               type="checkbox"
-               @change="toggleAll({ done: !allChecked })">
-        <label for="toggle-all" />
-        <ul class="todo-list">
-          <todo v-for="(todo, index) in filteredTodos"
-                :key="index"
-                :todo="todo"
-                @toggleTodo="toggleTodo"
-                @editTodo="editTodo"
-                @deleteTodo="deleteTodo" />
-        </ul>
-      </section>
-      <!-- footer -->
-      <footer v-show="todos.length"
-              class="footer">
-        <span class="todo-count">
-          <strong>{{ remaining }}</strong>
-          {{ remaining | pluralize('item') }} left
-        </span>
-        <ul class="filters">
-          <li v-for="(val, key) in filters"
-              :key="key">
-            <a :class="{ selected: visibility === key }"
-               @click.prevent="visibility = key">{{ key | capitalize }}</a>
+  <div class="listWrapper">
+    <!-- <div>
+      <draggable class="list-group"
+                 tag="ul"
+                 v-model="taskList"
+                 v-bind="dragOptions"
+                 :move="onMove"
+                 @start="isDragging=true"
+                 @end="isDragging=false">
+        <transition-group type="transition"
+                          :name="'flip-list'">
+
+          <li class="list-group-item"
+              v-for="element in taskList"
+              :key="element.role">
+            <i :class="element.fixed? 'fa fa-anchor' : 'glyphicon glyphicon-pushpin'"
+               @click=" element.fixed=! element.fixed"
+               aria-hidden="true"></i>
+            {{element.content}}
+            <span class="badge">{{element.role}}</span>
           </li>
-        </ul>
-        <!-- <button class="clear-completed" v-show="todos.length > remaining" @click="clearCompleted">
-        Clear completed
-      </button> -->
-      </footer>
-    </section>
+        </transition-group>
+      </draggable>
+    </div> -->
+    <div class="scroll">
+      <div class="undoneWrapper">
+        <draggable tag="ul"
+                   v-model="taskList"
+                   v-bind="dragOptions"
+                   :move="onMove"
+                   style=" height: inherit;">
+          <transition-group name="no"
+                            class="list-group"
+                            tag="ul">
+            <li class="list-group-item"
+                v-for="(element,index) in taskList"
+                :key="index">
+              <i :class="element.fixed? 'fa fa-anchor' : 'glyphicon glyphicon-pushpin'"
+                 @click=" element.fixed=! element.fixed"
+                 aria-hidden="true"></i>
+              {{element.content}}
+              <span class="badge">{{element.role}}</span>
+            </li>
+          </transition-group>
+        </draggable>
+      </div>
+      <div class="doneWrapper">
+        <draggable tag="ul"
+                   v-model="list2"
+                   v-bind="dragOptions"
+                   :move="onMove"
+                   style=" height: inherit;">
+          <transition-group name="no"
+                            class="list-group"
+                            tag="ul">
+            <li class="list-group-item"
+                v-for="(element,index) in list2"
+                :key="index">
+              <i :class="element.fixed? 'fa fa-anchor' : 'glyphicon glyphicon-pushpin'"
+                 @click=" element.fixed=! element.fixed"
+                 aria-hidden="true"></i>
+              {{element.content}}
+              <span class="badge">{{element.role}}</span>
+            </li>
+          </transition-group>
+        </draggable>
+      </div>
+
+    </div>
+
   </div>
-
 </template>
-
 <script>
-import Todo from './Todo.vue'
-
-const STORAGE_KEY = 'todos'
-const filters = {
-  all: todos => todos,
-  active: todos => todos.filter(todo => !todo.done),
-  completed: todos => todos.filter(todo => todo.done)
-}
-const defalutList = [
-  { text: 'star this repository', done: false },
-  { text: 'fork this repository', done: false },
-  { text: 'follow author', done: false },
-  { text: 'vue-element-admin', done: true },
-  { text: 'vue', done: true },
-  { text: 'element-ui', done: true },
-  { text: 'axios', done: true },
-  { text: 'webpack', done: true }
-]
+import draggable from 'vuedraggable'
 export default {
-  components: { Todo },
-  filters: {
-    pluralize: (n, w) => n === 1 ? w : w + 's',
-    capitalize: s => s.charAt(0).toUpperCase() + s.slice(1)
+  components: {
+    draggable
   },
   data () {
     return {
-      visibility: 'all',
-      filters,
-      // todos: JSON.parse(window.localStorage.getItem(STORAGE_KEY)) || defalutList
-      todos: defalutList
-    }
-  },
-  computed: {
-    allChecked () {
-      return this.todos.every(todo => todo.done)
-    },
-    filteredTodos () {
-      return filters[this.visibility](this.todos)
-    },
-    remaining () {
-      return this.todos.filter(todo => !todo.done).length
+      taskList: [
+        {
+          role: 1,
+          content: '去吃饭',
+          fixed: false
+        },
+        {
+          role: 2,
+          content: '去学习',
+          fixed: false
+        },
+        {
+          role: 3,
+          content: '去玩耍',
+          fixed: false
+        },
+        {
+          role: 4,
+          content: '是是是吃饭',
+          fixed: false
+        },
+        {
+          role: 4,
+          content: '是是是吃饭',
+          fixed: false
+        },
+        {
+          role: 4,
+          content: '是是是吃饭',
+          fixed: false
+        },
+        {
+          role: 4,
+          content: '是是是吃饭',
+          fixed: false
+        },
+        {
+          role: 4,
+          content: '是是是吃饭',
+          fixed: false
+        }
+      ],
+      isDragging: false,
+      editable: true,
+      delayedDragging: false,
+      list2: []
     }
   },
   methods: {
-    setLocalStorage () {
-      window.localStorage.setItem(STORAGE_KEY, JSON.stringify(this.todos))
+    addPeople () {
+
     },
-    addTodo (e) {
-      const text = e.target.value
-      if (text.trim()) {
-        this.todos.push({
-          text,
-          done: false
-        })
-        this.setLocalStorage()
+    onMove ({ relatedContext, draggedContext }) {
+      const relatedElement = relatedContext.element
+      const draggedElement = draggedContext.element
+      return (
+        (!relatedElement || !relatedElement.fixed) && !draggedElement.fixed
+      )
+    }
+  },
+  computed: {
+    dragOptions () {
+      return {
+        animation: 200,
+        group: 'description',
+        disabled: !this.editable,
+        ghostClass: 'ghost'
       }
-      e.target.value = ''
-    },
-    toggleTodo (val) {
-      val.done = !val.done
-      this.setLocalStorage()
-    },
-    deleteTodo (todo) {
-      this.todos.splice(this.todos.indexOf(todo), 1)
-      this.setLocalStorage()
-    },
-    editTodo ({ todo, value }) {
-      todo.text = value
-      this.setLocalStorage()
-    },
-    clearCompleted () {
-      this.todos = this.todos.filter(todo => !todo.done)
-      this.setLocalStorage()
-    },
-    toggleAll ({ done }) {
-      this.todos.forEach(todo => {
-        todo.done = done
-        this.setLocalStorage()
+    }
+  },
+  watch: {
+    isDragging (newValue) {
+      if (newValue) {
+        this.delayedDragging = true
+        return
+      }
+      this.$nextTick(() => {
+        this.delayedDragging = false
       })
     }
   }
 }
 </script>
 
-<style lang="scss">
-@import "./index.scss";
+<style scoped>
+.listWrapper {
+  position: relative;
+  margin-left: 200px;
+  overflow: hidden;
+  height: 100%;
+}
+.scroll {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: -20px;
+  bottom: 0;
+  display: flex;
+  flex-wrap: wrap;
+  overflow-y: scroll;
+  padding: 20px 60px 20px 40px;
+}
+.undoneWrapper,
+.doneWrapper {
+  width: 250px;
+  height: 300px;
+  flex: 0 0 250px;
+  margin: 10px;
+  background: white;
+  border-radius: 10px;
+  overflow: hidden;
+}
+@media screen and (max-width: 700px) {
+  .scroll {
+    justify-content: space-around;
+  }
+}
+.flip-list-move {
+  transition: transform 0.5s;
+}
+
+.no-move {
+  transition: transform 0s;
+}
+
+.ghost {
+  opacity: 0.5;
+  background: #c8ebfb;
+}
+
+.list-group {
+  min-height: 100%;
+}
+
+.list-group-item {
+  position: relative;
+  display: block;
+  padding: 0.75rem 1.25rem;
+  margin-bottom: -1px;
+  background-color: #fff;
+  border: 1px solid rgba(0, 0, 0, 0.125);
+  /* cursor: move; */
+}
+.list-group-item:first-child {
+  border-top-left-radius: 0.25rem;
+  border-top-right-radius: 0.25rem;
+}
+.list-group-item:last-child {
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
+}
+.list-group-item i {
+  cursor: pointer;
+}
+.list-group-item > .badge {
+  float: right;
+}
+.badge {
+  display: inline-block;
+  min-width: 10px;
+  padding: 3px 7px;
+  font-size: 12px;
+  font-weight: bold;
+  line-height: 1;
+  color: #fff;
+  text-align: center;
+  white-space: nowrap;
+  vertical-align: middle;
+  background-color: #777;
+  border-radius: 10px;
+}
+.glyphicon-pushpin:before {
+  content: "\E146";
+}
+.fa-anchor:before {
+  content: "\F13D";
+}
 </style>
