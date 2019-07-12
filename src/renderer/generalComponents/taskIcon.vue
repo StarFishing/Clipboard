ff55042556be3e4ac9e15286e318cc7b3c3cf00e
starfishing <2923627211@qq.com>
2019-07-12 16:48:27 +0800

    添加两个图标的交互

---
+++ src/renderer/generalComponents/taskIcon.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="taskicon kr-view"
+       @click='handler'
+       @mousedown="onmousedown"
+       @mouseup="onmouseup"
+       @mouseover="onmouseover"
+       @mouseout="onmouseout"
+       ref="taskwrapper">
+    <svg-icon icon-class="任务"
+              class="resizeclass"></svg-icon>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      isselect: false
+    }
+  },
+  methods: {
+    onmousedown () {
+      this.$refs.taskwrapper.style.transform = 'scaleX(0.9) scaleY(0.9)'
+      this.$refs.taskwrapper.style.opacity = '0.5'
+      this.$refs.taskwrapper.style['background-color'] = 'rgba(255, 255, 255, 0.3)'
+    },
+    onmouseup () {
+      this.$refs.taskwrapper.style.transform = 'scaleX(1) scaleY(1)'
+      this.$refs.taskwrapper.style.opacity = '1'
+      this.$refs.taskwrapper.style['background-color'] = 'rgba(255, 255, 255, 0.2)'
+    },
+    handler () {
+      this.isselect = !this.isselect
+      if (this.isselect) {
+        this.$refs.taskwrapper.style.opacity = '1'
+        this.$refs.taskwrapper.style['background-color'] = 'rgba(255, 255, 255, 0.2)'
+      } else {
+        this.$refs.taskwrapper.style['background-color'] = 'rgba(255, 255, 255, 0)'
+      }
+    },
+    onmouseover () {
+      this.$refs.taskwrapper.style.transform = 'scaleX(1.05) scaleY(1.05)'
+      this.$refs.taskwrapper.style['background-color'] = 'rgba(255, 255, 255, 0.2)'
+    },
+    onmouseout () {
+      if (this.isselect) {
+        this.$refs.taskwrapper.style.opacity = '1'
+        this.$refs.taskwrapper.style['background-color'] = 'rgba(255, 255, 255, 0.2)'
+      } else {
+        this.$refs.taskwrapper.style['background-color'] = 'rgba(255, 255, 255, 0)'
+      }
+      this.$refs.taskwrapper.style.transform = 'scaleX(1) scaleY(1)'
+    }
+  }
+}
+</script>
+<style scoped>
+@import url("./style.css");
+.taskicon {
+  width: 36px;
+  height: 36px;
+  align-items: center;
+  justify-content: center;
+  border-radius: 36px;
+  opacity: 1;
+  transition: all 0.3s;
+  background-color: rgba(255, 255, 255, 0);
+  transform: scaleX(1) scaleY(1);
+  cursor: pointer;
+}
+.resizeclass {
+  color: white;
+  width: 24px;
+  height: 24px;
+}
+</style>
---
+++ src/renderer/generalComponents/toggleButton.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="kr-view"
+       style="padding-left: 10px; align-items: center;">
+    <div class="kr-view wrapperCircle"
+         @click="toggle"
+         ref="wrapper">
+      <div class="kr-view cricleposition"
+           ref="circle"></div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      toggleflag: false
+    }
+  },
+  methods: {
+    toggle () {
+      this.toggleflag = !this.toggleflag
+      if (this.toggleflag) {
+        this.$refs.circle.style.transform = 'translateX(16px)'
+        this.$refs.wrapper.style.background = 'rgb(61, 204, 61)'
+      } else {
+        this.$refs.circle.style.transform = 'translateX(0)'
+        this.$refs.wrapper.style.background = 'rgb(213, 218, 220)'
+      }
+    }
+  }
+}
+</script>
+<style scoped>
+@import url("./style.css");
+.cricleposition {
+  width: 18px;
+  height: 18px;
+  position: absolute;
+  top: 3px;
+  left: 3px;
+  transition: all 0.3s ease;
+  border-radius: 100%;
+  background-color: white;
+  box-shadow: rgba(0, 0, 0, 0.05) 0px 0px 0px 1px;
+  transform: translateX(0);
+}
+.wrapperCircle {
+  transition: all 0.3s ease;
+  width: 40px;
+  height: 24px;
+  align-items: center;
+  border-radius: 999px;
+  cursor: pointer;
+  box-shadow: rgba(61, 71, 77, 0.1) 0px 0px 0px 1px inset;
+  background-color: rgb(213, 218, 220);
+}
+</style>
