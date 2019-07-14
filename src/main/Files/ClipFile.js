81186d16e08f371376e8b74fcd836e07dc0b1fc5
startfishing <2923627211@qq.com>
2019-07-14 14:13:58 +0800

    图标添加

--- src/main/Files/ClipFile.js
+++ src/main/Files/ClipFile.js
@@ -1,7 +1,7 @@
 // const fs = require('fs')
 // // fs.mkdir('D:Clipdata', { recursive: true }, err => {
 // //   if (err) throw err
-// fs.writeFile('./Clip.json', 'hello', 'utf8', err => {
+// fs.writeFile('/Users/xiaoxin/Desktop/Clip.css', 'hello', 'utf8', err => {
 //   if (err) throw err
 //   console.log('文件已被保存')
 // })
