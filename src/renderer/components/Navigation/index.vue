215bde53d8a9335926bbab6c6db6276a14af7ccb
startfishing <2923627211@qq.com>
2019-07-14 14:15:13 +0800

    换肤

--- src/renderer/components/Navigation/index.vue
+++ src/renderer/components/Navigation/index.vue
@@ -3,13 +3,19 @@
     <div id="mytitle"
          style="display:inline-block">
     </div>
-    <div class="action"
-         v-if="os">
+    <div class="action">
+      <div class="changebackgroud"
+           @click="changebackgroud">
+        <svg-icon icon-class="更新"
+                  style="width:22px;height:22px"></svg-icon>
+      </div>
       <div class="icon-mini"
+           v-if="os"
            @click="miniWindow">
         <span class="mini"></span>
       </div>
       <div class="icon-close"
+           v-if="os"
            @click="closeWindow">
         <span class="close"></span>
       </div>
@@ -26,7 +32,8 @@ export default {
   name: 'Mytitle',
   data () {
     return {
-      os: false
+      os: false,
+      toggle: false
     }
   },
   methods: {
@@ -35,6 +42,21 @@ export default {
     },
     miniWindow () {
       ipcRenderer.send('minwindow', { information: 'mini' })
+    },
+    changebackgroud () {
+      this.toggle = !this.toggle
+      const csstext = '#app{height:100%;background-image: url("https://img.pc841.com/2018/0730/20180730081702510.jpg");background-repeat: no-repeat;background-size: cover;}.homepage {background: none} .sidebarWrapper {background: none;}.navigationWrapper {background: none;}'
+      if (this.toggle) {
+        if (document.head.lastChild.innerText !== '') {
+          const style = document.createElement('style')
+          style.innerText = csstext
+          document.head.appendChild(style)
+        } else {
+          document.head.lastChild.innerText = csstext
+        }
+      } else {
+        document.head.lastChild.innerText = ''
+      }
     }
   },
   created () {
@@ -46,16 +68,6 @@ export default {
 </script>
     
 <style scoped>
-.navigationWrapper {
-  position: relative;
-  width: 100%;
-  height: 50px;
-  /* background-image: linear-gradient(135deg, #5efce8 10%, #736efe 100%); */
-  background-color: #7a5e87;
-  -webkit-app-region: drag;
-  border-radius: 0 25px 0 0;
-  /* box-shadow: 0 1px 10px rgba(0, 0, 0, 0.5); */
-}
 .action {
   display: flex;
   position: absolute;
@@ -66,7 +78,8 @@ export default {
   border-radius: 0 25px 0 0;
 }
 .icon-close,
-.icon-mini {
+.icon-mini,
+.changebackgroud {
   -webkit-app-region: no-drag;
   display: inline-block;
   cursor: pointer;
@@ -97,5 +110,17 @@ export default {
   width: 15px;
   height: 15px;
   background-image: url("../../assets/最小化.png");
+}
+</style>
+<style>
+.navigationWrapper {
+  position: relative;
+  width: 100%;
+  height: 50px;
+  /* background-image: linear-gradient(135deg, #5efce8 10%, #736efe 100%); */
+  background: #7a5e87;
+  -webkit-app-region: drag;
+  border-radius: 0 25px 0 0;
+  /* box-shadow: 0 1px 10px rgba(0, 0, 0, 0.5); */
 }
 </style>
