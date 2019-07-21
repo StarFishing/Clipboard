9f277c849dc97bb202c9b3ecf3297c445672b3f5
startfishing <2923627211@qq.com>
2019-07-21 17:55:14 +0800

    引入tooltip

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -3,7 +3,9 @@ import axios from 'axios'
 import App from './App'
 import router from './router'
 import store from './store'
+import VTooltip from 'v-tooltip'
 import './icons'
+Vue.use(VTooltip)
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
