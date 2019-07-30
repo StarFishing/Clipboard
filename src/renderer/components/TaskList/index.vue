0f02692c19fb7ffb76e92a30527707642057c29b
startfishing <2923627211@qq.com>
2019-07-30 16:11:34 +0800

    合并

--- src/renderer/components/TaskList/index.vue
+++ src/renderer/components/TaskList/index.vue
@@ -355,8 +355,8 @@ export default {
 .doneWrapper .taskitem {
   position: relative;
   height: 300px;
-  margin-right: -17px;
-  padding-right: 17px;
+  margin-right: -20px;
+  padding-right: 20px;
   overflow-y: scroll;
 }
 .controlitemWrapper {
@@ -364,7 +364,7 @@ export default {
   top: 0;
   left: 0;
   bottom: 0;
-  right: 0;
+  right: 20px;
   padding: 0 0 5px 0;
 }
 @media screen and (max-width: 700px) {
@@ -417,6 +417,20 @@ export default {
 }
 .list-group-item > .badge {
   float: right;
+}
+.badge {
+  display: inline-block;
+  min-width: 10px;
+  padding: 3px 7px;
+  font-size: 12px;
+  font-weight: bold;
+  line-height: 1;
+  color: #fff;
+  text-align: center;
+  white-space: nowrap;
+  vertical-align: middle;
+  background-color: #777;
+  border-radius: 10px;
 }
 /* 进入之前和离开之后的style */
 .add-enter {
@@ -472,8 +486,8 @@ export default {
 }
 .collpse-leave-to {
   opacity: 0;
-  transform: translateX(100px);
-  /* transform: scaleX(0) scaleY(0); */
+  /* transform: translateX(100px); */
+  transform: scaleX(0) scaleY(0);
 
   /* transform: translateX(100px); */
 }
