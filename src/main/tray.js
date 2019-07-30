f63347131b924e4517365c8f498b5126ad252f6d
starfishing <2923627211@qq.com>
2019-07-30 09:52:30 +0800

    更改图标

--- src/main/tray.js
+++ src/main/tray.js
@@ -15,10 +15,12 @@
   const { BrowserWindow, Tray } = require('electron')
   // BrowserWindow
   // 创建系统托盘
-  let tray = new Tray(path.resolve(__static, 'favicon-a.png'))
-  var win = BrowserWindow.getAllWindows()
-  tray.setToolTip('star pages')
-  tray.on('click', () => {
-    win[0].show()
-  })
+  let tray = new Tray(path.resolve(__static, 'favicon-l.png'))
+  return (function () {
+    var win = BrowserWindow.getAllWindows()
+    tray.setToolTip('star pages')
+    tray.on('click', () => {
+      win[0].show()
+    })
+  })()
 })()
